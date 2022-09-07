3f11a734d18c6d80dd536de3853c0cadab4b901d
Samuel Comte <samuel_comte@yahoo.fr>
2022-09-07 11:32:50 +0200

    home withOUT matplotlib

---
+++ srsf.py
@@ -0,0 +1,14 @@
+# SRSF : Simple Retail Sales Forecasting
+
+# print('Hey!') # goes to console
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="RSF App Start Page",
+    #page_icon="👋",
+    #layout="wide"
+)
+
+st.markdown("# SRSF")
+st.write("Simple Retail Sales Forecasting")
